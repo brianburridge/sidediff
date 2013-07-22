e607b34785ab887ebffc8b9ab6d087a3aaf55958
Brian Burridge <bburridg@gmail.com>
2013-07-22 11:41:47 -0400

    Added README. Refactored SideDiff file loading.

--- app/models/side_diff.rb
+++ app/models/side_diff.rb
@@ -2,28 +2,26 @@ class SideDiff
   require 'diff/lcs'
 
   def left_file
-    @left_file = []
-    File.open("public/left.rb", "r") do |infile|
-        while (line = infile.gets)
-          @left_file << line
-        end
-    end
-    return @left_file
+    get_file_content('public/left.rb')
   end
 
   def right_file
-    @right_file = []
-    File.open("public/right.rb", "r") do |infile|
-        while (line = infile.gets)
-          @right_file << line
-        end
-    end
-    return @right_file
+    get_file_content('public/right.rb')
   end
 
   def compare_files
     Diff::LCS.sdiff(left_file, right_file)
   end
 
+  private
 
+  def get_file_content(filename)
+    content = []
+    File.open(filename, "r") do |infile|
+        while (line = infile.gets)
+          content << line
+        end
+    end
+    return content
+  end
 end
