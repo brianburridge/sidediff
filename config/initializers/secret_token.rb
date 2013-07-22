f556b34687737c3733213b4e29d61276454d53c9
Brian Burridge <bburridg@gmail.com>
2013-07-22 09:45:04 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sidediff::Application.config.secret_token = 'd869fbc78174ca40fe0c4cbc19b865beff59ab80de361fa07ff1343159ee1c7c973e8c69351c35fc5a9e4147d329f1ee2d35ca2c9f9a974bdf93f7926ac15b96'
